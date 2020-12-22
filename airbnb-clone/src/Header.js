716c9de09df9e1b42a063832091327d144be311b
Jessdev <61420136+JessimH@users.noreply.github.com>
2020-12-22 11:40:37 +0100

    first commit

---
+++ airbnb-clone/src/Header.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import './Header.css'
+import { Avatar } from '@material-ui/core'
+import SearchOutlinedIcon from '@material-ui/icons/SearchOutlined';
+import ExpandMoreOutlinedIcon from '@material-ui/icons/ExpandMoreOutlined';
+import LanguageIcon from '@material-ui/icons/Language';
+
+function Header() {
+    return (
+        <div className="header">
+            <img
+                className="header__icon"
+                src="https://i.pinimg.com/originals/3c/bf/be/3cbfbe148597341fa56f2f87ade90956.png"
+                alt=""
+            />
+
+            <div className="header__center">
+                <input type="text" />
+                <SearchOutlinedIcon />
+            </div>
+
+            <div className="header__right">
+                <p>Become a host</p>
+                <LanguageIcon />
+                <ExpandMoreOutlinedIcon />
+                <Avatar />
+            </div>
+
+        </div>
+    )
+}
+
+export default Header
