bb6e442bde396f2d5e678321aab080d2bacda2fa
Jessdev <61420136+JessimH@users.noreply.github.com>
2020-12-22 17:55:11 +0100

    creation SearchPage component

--- airbnb-clone/src/SearchPage.js
+++ airbnb-clone/src/SearchPage.js
@@ -4,7 +4,9 @@ import './SearchPage.css'
 function SearchPage() {
     return (
         <div className="searchPage">
-            <h1>Search Page</h1>
+            <div className="searchPage__info">
+
+            </div>
         </div>
     )
 }
