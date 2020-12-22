3d3e7f252d6a4d0b0ed5efe393096b5fd911f164
Jessdev <61420136+JessimH@users.noreply.github.com>
2020-12-22 15:22:21 +0100

    React Router (Switch)

--- airbnb-clone/src/App.js
+++ airbnb-clone/src/App.js
@@ -3,18 +3,28 @@ import React from 'react'
 import Home from './Home'
 import Header from './Header'
 import Footer from './Footer'
+import SearchPage from './SearchPage'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="app">
-      <Header />
-      <Home />
-      <Footer />
-      {/* Footer */}
 
-      {/* SearchPage */}
-      {/* Header */}
-      {/*Search  */}
+    <div className="app">
+      <Router>
+        <Header />
+
+        <Switch>
+          <Route path="/search">
+            <SearchPage />
+          </Route>
+
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+
+        <Footer />
+      </Router>
     </div>
   );
 }
