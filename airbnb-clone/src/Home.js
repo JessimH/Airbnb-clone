bd7991f43695ffe259ce26bd12afc4334a25fd0e
Jessdev <61420136+JessimH@users.noreply.github.com>
2020-12-22 14:32:13 +0100

    Start Cards

--- airbnb-clone/src/Home.js
+++ airbnb-clone/src/Home.js
@@ -8,9 +8,21 @@ function Home() {
         <div className="home">
             <Banner />
             <div className="home__section">
-                <Card/>
-                <Card/>
-                <Card/>
+                <Card
+                    src="https://www.challenges.fr/assets/img/2018/03/09/cover-r4x3w1000-5cf543474ae31-san-francisco-6866584-living-room.jpg"
+                    title="Entire Homes"
+                    description="Comfortable private places, with romm for friends or family."
+                />
+                <Card
+                    src="https://beyondphilosophy.com/wp-content/uploads/2019/10/0.jpg"
+                    title="Entire Homes"
+                    description="Comfortable private places, with romm for friends or family."
+                />
+                <Card
+                    src="https://www.fodors.com/wp-content/uploads/2019/08/airbnb-hero-.jpg"
+                    title="Entire Homes"
+                    description="Comfortable private places, with romm for friends or family."
+                />
             </div>
         </div>
     )
